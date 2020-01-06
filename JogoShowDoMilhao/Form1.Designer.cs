4e7682b1cfd0836859ac30ad7ef7fc1073b32694
Fernanda Winter <fernanda.winter@hotmail.com>
2020-01-05 23:45:09 -0300

    Adicionando mais perguntas e tornando as mesmas randômicas

--- JogoShowDoMilhao/Form1.Designer.cs
+++ JogoShowDoMilhao/Form1.Designer.cs
@@ -29,76 +29,76 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.label1 = new System.Windows.Forms.Label();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.lblQuestionDescription = new System.Windows.Forms.Label();
+            this.rdbOption1 = new System.Windows.Forms.RadioButton();
+            this.rdbOption2 = new System.Windows.Forms.RadioButton();
+            this.rdbOption3 = new System.Windows.Forms.RadioButton();
+            this.rdbOption4 = new System.Windows.Forms.RadioButton();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // lblQuestionDescription
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Gold;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(176, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(389, 25);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Em qual país está a Torre de Pisa ?";
+            this.lblQuestionDescription.AutoSize = true;
+            this.lblQuestionDescription.BackColor = System.Drawing.Color.Gold;
+            this.lblQuestionDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestionDescription.Location = new System.Drawing.Point(176, 9);
+            this.lblQuestionDescription.Name = "lblQuestionDescription";
+            this.lblQuestionDescription.Size = new System.Drawing.Size(389, 25);
+            this.lblQuestionDescription.TabIndex = 0;
+            this.lblQuestionDescription.Text = "Em qual país está a Torre de Pisa ?";
             // 
-            // radioButton1
+            // rdbOption1
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton1.Location = new System.Drawing.Point(26, 54);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(70, 22);
-            this.radioButton1.TabIndex = 1;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Grécia";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            this.rdbOption1.AutoSize = true;
+            this.rdbOption1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbOption1.Location = new System.Drawing.Point(26, 54);
+            this.rdbOption1.Name = "rdbOption1";
+            this.rdbOption1.Size = new System.Drawing.Size(70, 22);
+            this.rdbOption1.TabIndex = 1;
+            this.rdbOption1.TabStop = true;
+            this.rdbOption1.Text = "Grécia";
+            this.rdbOption1.UseVisualStyleBackColor = true;
+            this.rdbOption1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
             // 
-            // radioButton2
+            // rdbOption2
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton2.Location = new System.Drawing.Point(26, 100);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(132, 22);
-            this.radioButton2.TabIndex = 2;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Estados Unidos";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            this.rdbOption2.AutoSize = true;
+            this.rdbOption2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbOption2.Location = new System.Drawing.Point(26, 100);
+            this.rdbOption2.Name = "rdbOption2";
+            this.rdbOption2.Size = new System.Drawing.Size(132, 22);
+            this.rdbOption2.TabIndex = 2;
+            this.rdbOption2.TabStop = true;
+            this.rdbOption2.Text = "Estados Unidos";
+            this.rdbOption2.UseVisualStyleBackColor = true;
+            this.rdbOption2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
             // 
-            // radioButton3
+            // rdbOption3
             // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton3.Location = new System.Drawing.Point(26, 145);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(55, 22);
-            this.radioButton3.TabIndex = 3;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "Itália";
-            this.radioButton3.UseVisualStyleBackColor = true;
-            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            this.rdbOption3.AutoSize = true;
+            this.rdbOption3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbOption3.Location = new System.Drawing.Point(26, 145);
+            this.rdbOption3.Name = "rdbOption3";
+            this.rdbOption3.Size = new System.Drawing.Size(55, 22);
+            this.rdbOption3.TabIndex = 3;
+            this.rdbOption3.TabStop = true;
+            this.rdbOption3.Text = "Itália";
+            this.rdbOption3.UseVisualStyleBackColor = true;
+            this.rdbOption3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
             // 
-            // radioButton4
+            // rdbOption4
             // 
-            this.radioButton4.AutoSize = true;
-            this.radioButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton4.Location = new System.Drawing.Point(26, 194);
-            this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(72, 22);
-            this.radioButton4.TabIndex = 4;
-            this.radioButton4.TabStop = true;
-            this.radioButton4.Text = "França";
-            this.radioButton4.UseVisualStyleBackColor = true;
-            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            this.rdbOption4.AutoSize = true;
+            this.rdbOption4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbOption4.Location = new System.Drawing.Point(26, 194);
+            this.rdbOption4.Name = "rdbOption4";
+            this.rdbOption4.Size = new System.Drawing.Size(72, 22);
+            this.rdbOption4.TabIndex = 4;
+            this.rdbOption4.TabStop = true;
+            this.rdbOption4.Text = "França";
+            this.rdbOption4.UseVisualStyleBackColor = true;
+            this.rdbOption4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
             // 
             // button1
             // 
@@ -119,13 +119,13 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.radioButton4);
-            this.Controls.Add(this.radioButton3);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rdbOption4);
+            this.Controls.Add(this.rdbOption3);
+            this.Controls.Add(this.rdbOption2);
+            this.Controls.Add(this.rdbOption1);
+            this.Controls.Add(this.lblQuestionDescription);
             this.Name = "Form1";
-            this.Text = "SHOW DO MILHÃO";
+            this.Text = "QUESTÃO 01";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -133,11 +133,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton3;
-        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Label lblQuestionDescription;
+        private System.Windows.Forms.RadioButton rdbOption1;
+        private System.Windows.Forms.RadioButton rdbOption2;
+        private System.Windows.Forms.RadioButton rdbOption3;
+        private System.Windows.Forms.RadioButton rdbOption4;
         private System.Windows.Forms.Button button1;
     }
 }
--- JogoShowDoMilhao/Form1.cs
+++ JogoShowDoMilhao/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JogoShowDoMilhao.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,20 +13,78 @@ namespace JogoShowDoMilhao
 {
     public partial class Form1 : Form
     {
+        List<Question> availableQuestions = new List<Question>();
+
+        Question randomQuestion;
+
         public Form1()
         {
             InitializeComponent();
+
+            availableQuestions.Add(new Question()
+            {
+                QuestionDescription = "Onde fica a cidade de Feira de Santana?",
+                Option1 = "Pernambuco",
+                Option2 = "Bahia",
+                Option3 = "Sergipe",
+                Option4 = "Alagoas",
+                CorrectAnswer = 2,
+                DifficultyLevel = "Fácil"
+            });
+
+            availableQuestions.Add(new Question()
+            {
+                QuestionDescription = "O falcão é ...",
+                Option1 = "Réptil",
+                Option2 = "Ave",
+                Option3 = "Anfíbio",
+                Option4 = "Mamífero",
+                CorrectAnswer = 2,
+                DifficultyLevel = "Fácil"
+            });
+
+            availableQuestions.Add(new Question()
+            {
+                QuestionDescription = "Trompa de falópio é ...",
+                Option1 = "Pequeno trombone",
+                Option2 = "Esforço de guerra",
+                Option3 = "Barulho ensurdecedor",
+                Option4 = "Órgão feminino",
+                CorrectAnswer = 4,
+                DifficultyLevel = "Fácil"
+            });
+
+            Random rnd = new Random();
+            int r = rnd.Next(availableQuestions.Count);
+     
+            randomQuestion = availableQuestions[r];
+
+            lblQuestionDescription.Text = randomQuestion.QuestionDescription;
+            rdbOption1.Text = randomQuestion.Option1;
+            rdbOption2.Text = randomQuestion.Option2;
+            rdbOption3.Text = randomQuestion.Option3;
+            rdbOption4.Text = randomQuestion.Option4;
         }
 
-        int resposta = 0;
+        int userAnswer = 0;
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (resposta == 1)
+            if (userAnswer == randomQuestion.CorrectAnswer)
             {
                 MessageBox.Show("Certa resposta!");
-            }
 
+                Random rnd = new Random();
+                int r = rnd.Next(availableQuestions.Count);
+
+                randomQuestion = availableQuestions[r];
+
+                lblQuestionDescription.Text = randomQuestion.QuestionDescription;
+                rdbOption1.Text = randomQuestion.Option1;
+                rdbOption2.Text = randomQuestion.Option2;
+                rdbOption3.Text = randomQuestion.Option3;
+                rdbOption4.Text = randomQuestion.Option4;
+            }
             else
             {
                 MessageBox.Show("Que pena! Resposta errada!");
@@ -34,22 +93,22 @@ namespace JogoShowDoMilhao
 
         private void radioButton3_CheckedChanged(object sender, EventArgs e)
         {
-            resposta = 1;
+            userAnswer = 3;
         }
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
-            resposta = 2;
+            userAnswer = 1;
         }
 
         private void radioButton2_CheckedChanged(object sender, EventArgs e)
         {
-            resposta = 3;
+            userAnswer = 2;
         }
 
         private void radioButton4_CheckedChanged(object sender, EventArgs e)
         {
-            resposta = 4;
+            userAnswer = 4;
         }
     }
 }
